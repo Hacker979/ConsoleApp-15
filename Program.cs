9e2208e279e9e30663fb963f380df4c7705a3ede
Kab-35-16 <Kab-35-16@KAB-31-11->
2024-12-24 08:46:02 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_15
+{
+    class Program
+    {
+        static void print()
+        {
+
+        }
+
+        /// <summary>
+        /// Задача 1. Напечатать ряд чисел 20 в виде: 20 20 20 20 20 20 20 20 20 20.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
